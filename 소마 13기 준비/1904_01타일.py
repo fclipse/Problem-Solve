333e4279b8ede2ecf300baf04a0f68fa593c2f39
unknown <hsj0209@konkuk.ac.kr>
2022-03-17 11:13:30 +0900

    1904 added

--- 소마 13기 준비/1904_01타일.py
+++ 소마 13기 준비/1904_01타일.py
@@ -1,2 +1,9 @@
 n = int(input())
-lst = [0] * n
+lst = [1, 2, 0]
+for i in range(2, n):
+    lst[2] = (lst[1] + lst[0]) % 15746
+    lst[0], lst[1] = lst[1], lst[2]
+if n < 3:
+    print(lst[n-1])
+else:
+    print(lst[-1])
