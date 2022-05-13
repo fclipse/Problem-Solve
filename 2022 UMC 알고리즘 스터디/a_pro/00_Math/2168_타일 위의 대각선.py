2ce54b0d4232920c33f09fdeeab74ff3b3dc5ebe
siejwkaodj <54460736+siejwkaodj@users.noreply.github.com>
2022-05-13 22:10:51 +0900

    0513 / siejwkaodj

--- 2022 UMC 알고리즘 스터디/a_pro/00_Math/2168_타일 위의 대각선.py
+++
@@ -1,8 +0,0 @@
-# 시간 초과...
-import math, sys
-x, y = map(int, sys.stdin.readline().split())
-M = y / x
-sum_blocks = 0
-for i in range(x):
-    sum_blocks += math.ceil(M * (i+1) -int(M * i))
-print(sum_blocks)
--- 2022 UMC 알고리즘 스터디/a_pro/00_Math/2553_마지막 팩토리얼 수.py
+++
@@ -1,8 +0,0 @@
-# 20000 이하의 수 n을 받아 그 수의 팩토리얼에서 0이 아닌 가장 낮을 자리를 
-# 출력하는 문제
-import math
-n = int(input())
-for i in reversed(str(math.factorial(n))):
-    if i != '0':
-        print(i)
-        break
